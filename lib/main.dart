7eaf7b2ab6bee1a2fc94a04a20be5d245646d8fe
oluseunDesina <oadesina@secureodltd.com>
2021-06-29 18:14:40 +0100

    starting sign in screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,115 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(Login());
+}
+
+class Welcome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: Center(
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: SafeArea(
+          child: Center(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 40.0),
+                    child: RaisedButton(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text('Sign In',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 15)),
+                      ),
+                      onPressed: () {},
+                      elevation: 5,
+                      color: Color.fromRGBO(37, 148, 0, 1),
+                      // color: Color(0x269400),
+                      textColor: Colors.white,
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromRGBO(37, 148, 0, 1),
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(37, 148, 0, 1),
+          leading: Icon(
+            Icons.keyboard_arrow_left, //change to custom Icon
+            // color: Colors.pink,
+            size: 24.0,
+            semanticLabel: 'Text to announce in accessibility modes',
+          ),
+          elevation: 0,
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                RaisedButton(
-                  child: Text('Create Account',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 15)),
-                  onPressed: () {},
-                  elevation: 5,
-                  color: Colors.white,
-                  textColor: Colors.green,
+                Text(
+                  "Sign In",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.normal,
+                      color: Colors.white),
                 ),
-                RaisedButton(
-                  child: Text('Sign In',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 15)),
-                  onPressed: () {},
-                  elevation: 5,
-                  color: Colors.green,
-                  textColor: Colors.white,
+                SizedBox(
+                  height: 10,
                 ),
+                Text(
+                  "Welcome back. Securely sign in to your Stamp account",
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      color: Color.fromRGBO(174, 227, 156, 1)),
+                ),
+                Text(
+                  "Surname",
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      color: Color.fromRGBO(255, 255, 255, 1)),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), labelText: 'Enter Surname'),
+                )
               ],
             ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
